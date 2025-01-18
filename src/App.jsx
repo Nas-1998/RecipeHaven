a6df1a88116f4c8b1ce1e3d0f280a0494e9ff120
nas1998 <hassan.nas1998@gmail.com>
2025-01-18 00:30:45 +0000

    added vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Recipe Haven</h1>
+      <p>Your ultimate recipe management tool!</p>
+    </div>
+  );
+};
+
+export default App;
